25ada209aa93e83df67c61a56b9c3363624d9b9e
John McK <john.mckech3@gmail.com>
2020-06-14 17:37:10 -0500

    adding generate function

--- develop/script.js
+++ develop/script.js
@@ -4,10 +4,23 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generate() {
+  let complexity = document.getElementById("password").value;
+
+  //possible values
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
+
+  let password = "";
+
+  //create for loop to choose pw characters
+  for(var i = 0; i<= complexity; i++) {
+      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
+
+  //add password to textbox
+  document.getElementById("password").value = password;
 
   passwordText.value = password;
 
